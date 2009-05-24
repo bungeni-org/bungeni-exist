5e3be83f355af660562b70a0cd8018bbb4b19622
adam.retter <adam.retter@25fa2e06-0413-11de-b4d5-6978306beb92>
2009-05-24 17:07:10 +0000

    Refactoring and javadoc

--- trunk/BackupConverter/src/org/bungeni/exist/tools/backupconverter/writer/ANWriter.java
+++ trunk/BackupConverter/src/org/bungeni/exist/tools/backupconverter/writer/ANWriter.java
@@ -8,18 +8,39 @@ import org.bungeni.exist.tools.backupconverter.backup.Resource;
 import java.io.IOException;
 
 /**
+ * Akoma Ntoso Writer
+ * Responsible for writting Items to to an Akoma Ntoso destination
+ *
  * @author Adam Retter <adam.retter@googlemail.com>
  * @version 1.0
  */
 public interface ANWriter
 {
-
-
+    /**
+     * Writes a Collection
+     *
+     * @param collection The Collection to write
+     */
     public void writeCollection(Collection collection) throws IOException;
 
+    /**
+     * Writes a Resource
+     *
+     * @param resource The Resource to write
+     */
     public void writeResource(Resource resource) throws IOException;
 
-    public void removeCollection(Item resource) throws IOException;
+    /**
+     * Removes a Collection
+     *
+     * @param collection The Collection to remove
+     */
+    public void removeCollection(Item collection) throws IOException;
 
+    /**
+     * Removes a Resource
+     *
+     * @param resource The Resource to remove
+     */
     public void removeResource(Item resource) throws IOException;
 }

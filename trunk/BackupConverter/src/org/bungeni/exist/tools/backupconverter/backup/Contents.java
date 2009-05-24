3c75a7c3eaa3aa292a16b2dc1cb6204cff0b571b
adam.retter <adam.retter@25fa2e06-0413-11de-b4d5-6978306beb92>
2009-05-24 16:22:12 +0000

    Refactoring and javadoc

--- trunk/BackupConverter/src/org/bungeni/exist/tools/backupconverter/backup/Contents.java
+++ trunk/BackupConverter/src/org/bungeni/exist/tools/backupconverter/backup/Contents.java
@@ -14,22 +14,31 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 /**
+ * Represents a __contents__.xml Collection Descriptor in the Backup
+ *
  * @author Adam Retter <adam.retter@googlemail.com>
  * @version 1.0
  */
 public class Contents extends Item
 {
     private final InputStream inputStream;
-
     private List<ContentsEntry> entries = null;
 
-    
+    /**
+     * @param path The Path of the Contents
+     * @param inputStream The input stream for reading the Contents
+     */
     public Contents(String path, InputStream inputStream)
     {
         super(path);
         this.inputStream = inputStream;
     }
 
+    /**
+     * Gets the entries in the Contents
+     *
+     * @return List of Contents entries
+     */
     public List<ContentsEntry> getEntries() throws IOException
     {
         if(entries == null)
@@ -38,6 +47,10 @@ public class Contents extends Item
         return entries;
     }
 
+    /**
+     * SAX parses a __contents__.xml document
+     * and extracts the entries
+     */
     private void parseContentsDocument() throws IOException
     {
         try
@@ -56,11 +69,18 @@ public class Contents extends Item
         }
     }
 
+    /**
+     * Represents an entry in the __contents__.xml file
+     */
     public class ContentsEntry extends Item
     {
-        public ContentsEntry(Contents contents, String name)
+        /**
+         * @param contents The Contents from which this entry was extracted
+         * @param filename The filename of the file represented by this Contents Entry
+         */
+        public ContentsEntry(Contents contents, String filename)
         {
-            super(new File(new File(contents.getPath()).getParentFile(), name).getPath());
+            super(new File(new File(contents.getPath()).getParentFile(), filename).getPath());
         }
 
         @Override
@@ -70,11 +90,18 @@ public class Contents extends Item
         }
     }
 
+    /**
+     * Represents a <subcollection/> entry in the __contents__.xml file
+     */
     public class ContentsSubCollectionEntry extends ContentsEntry
     {
-        public ContentsSubCollectionEntry(Contents contents, String name)
+        /**
+         * @param contents The Contents from which this sub collection entry was extracted
+         * @param filename The filename of the sub collection represented by this Contents Entry
+         */
+        public ContentsSubCollectionEntry(Contents contents, String filename)
         {
-            super(contents, name);
+            super(contents, filename);
         }
 
         @Override
@@ -84,11 +111,18 @@ public class Contents extends Item
         }
     }
 
+    /**
+     * Represents a <resource/> entry in the __contents__.xml file
+     */
     public class ContentsResourceEntry extends ContentsEntry
     {
-        public ContentsResourceEntry(Contents contents, String name)
+        /**
+         * @param contents The Contents from which this resource entry was extracted
+         * @param filename The filename of the resource represented by this Contents Entry
+         */
+        public ContentsResourceEntry(Contents contents, String filename)
         {
-            super(contents, name);
+            super(contents, filename);
         }
 
         @Override
@@ -98,7 +132,10 @@ public class Contents extends Item
         }
     }
 
-
+    /**
+     * SAX Content Handler which extracts the SubCollection and Resource entries
+     * from a Contents
+     */
     private class ContentsContentHandler extends DefaultHandler
     {
         private final static String EXIST_NS = "http://exist.sourceforge.net/NS/exist";
@@ -110,6 +147,9 @@ public class Contents extends Item
         private List<ContentsEntry> entries = new ArrayList<ContentsEntry>();
         
 
+        /**
+         * @param contents The Contents that we are parsing
+         */
         public ContentsContentHandler(Contents contents)
         {
             this.contents = contents;
@@ -118,17 +158,20 @@ public class Contents extends Item
         @Override
         public void startElement(String uri, String localName, String qName, Attributes attrs) throws SAXException
         {
+            //get the filename for the entry
             String filename = null;
             if(attrs != null)
             {
                 filename = attrs.getValue(FILENAME_ATTR_NAME);
             }
 
+            //subcollection entry?
             if(localName.equals(SUBCOLLECTION_ELEMENT_NAME) && uri.equals(EXIST_NS))
             {
                 if(filename != null)
                     entries.add(new ContentsSubCollectionEntry(contents, filename));
             }
+            //resource entry
             else if(localName.equals(RESOURCE_ELEMENT_NAME) && uri.equals(EXIST_NS))
             {
                 if(filename != null)
@@ -136,6 +179,11 @@ public class Contents extends Item
             }
         }
 
+        /**
+         * Gets the parsed entries
+         *
+         * @return entries from the Contents
+         */
         public List<ContentsEntry> getEntries()
         {
             return entries;

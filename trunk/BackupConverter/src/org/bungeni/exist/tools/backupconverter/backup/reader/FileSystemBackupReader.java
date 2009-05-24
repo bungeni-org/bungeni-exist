fcb967b49852b74a4ca34f53b08fa88b07c00b8c
adam.retter <adam.retter@25fa2e06-0413-11de-b4d5-6978306beb92>
2009-05-24 16:51:04 +0000

    Refactoring and javadoc

--- trunk/BackupConverter/src/org/bungeni/exist/tools/backupconverter/backup/reader/FileSystemBackupReader.java
+++ trunk/BackupConverter/src/org/bungeni/exist/tools/backupconverter/backup/reader/FileSystemBackupReader.java
@@ -17,28 +17,61 @@ import java.util.Collections;
 import java.util.List;
 
 /**
+ * BackupReader for reading an eXist Backup from the file system
+ *
  * @author Adam Retter <adam.retter@googlemail.com>
  * @version 1.0
  */
 public class FileSystemBackupReader extends BackupReader
 {
-    private final String backupSrcPath;
+    private final String backupFolderPath;
 
-    public FileSystemBackupReader(File backupSrc)
+    /**
+     * @param backupFolder The backup folder that we are reading
+     */
+    public FileSystemBackupReader(File backupFolder)
     {
-        super(backupSrc);
-        backupSrcPath = backupSrc.getAbsolutePath();
+        super(backupFolder);
+        backupFolderPath = backupFolder.getAbsolutePath();
     }
 
+    
+    @Override
+    protected List<Item> getBackupItems() throws IOException
+    {
+        if(this.backupItems == null)
+        {
+            //load the items from the backup
+            this.backupItems = loadItemsRecursive(backupSrc, new ArrayList<Item>());
+
+            //sort the items into A-Z path order
+            Collections.sort(backupItems, new ItemPathComparator());
+        }
+
+        return this.backupItems;
+    }
+
+    /**
+     * Recursivley moves through the backup folder and loads the items from the backup
+     *
+     * @param f The folder to load the items from
+     * @param items The cumulatively loaded items
+     *
+     * @return List of Items loaded from the backup
+     */
     private List<Item> loadItemsRecursive(File f, List<Item> items) throws IOException
     {
-        String path = getPathRelativeToBackupSrc(f);
+        String path = getPathRespectingBackupFolder(f);
 
         if(f.isDirectory())
         {
             if(path.length() != 0)
+            {
+                //collection
                 items.add(new Collection(path));
+            }
 
+            //recurse
             String children[] = f.list();
             for(String child : children)
             {
@@ -49,10 +82,12 @@ public class FileSystemBackupReader extends BackupReader
         {
             if(path.endsWith(BackupDescriptor.COLLECTION_DESCRIPTOR))
             {
+                //contents
                 items.add(new Contents(path, new FileInputStream(f)));
             }
             else
             {
+                //resource
                 items.add(new Resource(path, new FileInputStream(f)));
             }
         }
@@ -60,27 +95,22 @@ public class FileSystemBackupReader extends BackupReader
         return items;
     }
 
-    private String getPathRelativeToBackupSrc(File f)
+    /**
+     * Gets the path of file backupFile relative to the backup folder
+     *
+     * @param backupFile A file from the Backup
+     *
+     * @return The absolute path of backupFile with respect to the backup folder
+     */
+    private String getPathRespectingBackupFolder(File backupFile)
     {
-        String path = f.getPath();
-        path = path.substring(path.indexOf(backupSrcPath) + backupSrcPath.length());
+        String path = backupFile.getPath();
+        path = path.substring(path.indexOf(backupFolderPath) + backupFolderPath.length());
 
         if(path.startsWith(PATH_SEPARATOR))
             path = path.substring(1);
-        
+
         return path;
-    }
-
-    @Override
-    protected List<Item> getBackupItems() throws IOException
-    {
-        if(this.backupItems == null)
-        {
-            this.backupItems = loadItemsRecursive(backupSrc, new ArrayList<Item>());
-            Collections.sort(backupItems, new ItemPathComparator());
-        }
-
-        return this.backupItems;
     }
 
     @Override

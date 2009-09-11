19334d7ac3b9babb7ca45614c47bf024d1d95810
adam.retter <adam.retter@25fa2e06-0413-11de-b4d5-6978306beb92>
2009-09-11 19:56:50 +0000

    Further tests

--- trunk/RESTTestSuite/test/org/bungeni/exist/query/QueryTest.java
+++ trunk/RESTTestSuite/test/org/bungeni/exist/query/QueryTest.java
@@ -14,14 +14,12 @@ import org.apache.commons.httpclient.methods.DeleteMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.jxpath.JXPathContext;
 import org.apache.commons.jxpath.ri.QName;
-import org.apache.commons.jxpath.ri.model.NodeIterator;
 import org.apache.commons.jxpath.ri.model.dom.DOMNodePointer;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import org.xml.sax.SAXException;
 
 /**
@@ -47,7 +45,7 @@ public class QueryTest
         packageTest.store_package_ke_act_1993_12_16_9();
         storedEntryPaths = packageTest.getStoredEntryPaths();
     }
-
+/*
     @AfterClass
     public static void removeTestPackage() throws IOException
     {
@@ -58,7 +56,7 @@ public class QueryTest
             int result = client.executeMethod(delete);
         }
     }
-
+*/
     @Test
     public void listWorkComponents() throws IOException, SAXException, ParserConfigurationException
     {
@@ -111,6 +109,110 @@ public class QueryTest
         ptrMatch = itPtrMatch.next();
         assertEquals("/ke/act/1993-12-31/9/eng@/main/schedule01.xml", getAttributeValue("an-manifestation-uri", ptrMatch));
         assertEquals("/db/bungeni/data/ke/act/1993/12-16_9_eng_annex1.xml", getAttributeValue("db-uri", ptrMatch));
+
+        try
+        {
+            itPtrMatch.next();
+        }
+        catch(Exception e)
+        {
+            assertEquals(NoSuchElementException.class, e.getClass());
+        }
+
+    }
+
+    @Test
+    public void listExpressionComponents() throws IOException, SAXException, ParserConfigurationException
+    {
+        final String queryAction = "list-components";
+        final String queryUri = "/ke/act/1993-12-31/9/eng@";
+
+
+        GetMethod get = new GetMethod(REST.QUERY_URL);
+        NameValuePair qsParams[] = {
+            new NameValuePair("action", queryAction),
+            new NameValuePair("uri", queryUri)
+        };
+        get.setQueryString(qsParams);
+
+        HttpClient client = REST.getAuthenticatingHttpClient(Database.DEFAULT_ADMIN_USERNAME, Database.DEFAULT_ADMIN_PASSWORD);
+
+        int result = client.executeMethod(get);
+
+        assertEquals(HttpStatus.SC_OK, result);
+
+        DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
+        Document docResult = builder.parse(get.getResponseBodyAsStream());
+
+        JXPathContext jxp = JXPathContext.newContext(docResult);
+        jxp.setLenient(true);
+        jxp.registerNamespace(QUERY_NAMESPACE_PREFIX, QUERY_NAMESPACE_URI);
+        jxp.registerNamespace(AkomaNtoso.AN_NAMESPACE_PREFIX, AkomaNtoso.AN_NAMESPACE_URI);
+
+        //assertions
+        String responseRequestAction = (String)jxp.getValue("/query:query/query:request/query:action");
+        assertEquals(queryAction, responseRequestAction);
+
+        String responseRequestUri = (String)jxp.getValue("/query:query/query:request/query:params/an:uri");
+        assertEquals(queryUri, responseRequestUri);
+
+        Iterator<DOMNodePointer> itPtrMatch = (Iterator<DOMNodePointer>)jxp.iteratePointers("/query:query/query:results/query:match");
+
+        DOMNodePointer ptrMatch = itPtrMatch.next();
+        assertEquals("/ke/act/1993-12-31/9/eng@/main.xml", getAttributeValue("an-manifestation-uri", ptrMatch));
+        assertEquals("/db/bungeni/data/ke/act/1993/12-16_9_eng_main.xml", getAttributeValue("db-uri", ptrMatch));
+
+        try
+        {
+            itPtrMatch.next();
+        }
+        catch(Exception e)
+        {
+            assertEquals(NoSuchElementException.class, e.getClass());
+        }
+
+    }
+
+    @Test
+    public void listManifestationComponents() throws IOException, SAXException, ParserConfigurationException
+    {
+        final String queryAction = "list-components";
+        final String queryUri = "/ke/act/1993-12-31/9/eng@.akn";
+
+
+        GetMethod get = new GetMethod(REST.QUERY_URL);
+        NameValuePair qsParams[] = {
+            new NameValuePair("action", queryAction),
+            new NameValuePair("uri", queryUri)
+        };
+        get.setQueryString(qsParams);
+
+        HttpClient client = REST.getAuthenticatingHttpClient(Database.DEFAULT_ADMIN_USERNAME, Database.DEFAULT_ADMIN_PASSWORD);
+
+        int result = client.executeMethod(get);
+
+        assertEquals(HttpStatus.SC_OK, result);
+
+        DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
+        Document docResult = builder.parse(get.getResponseBodyAsStream());
+
+        JXPathContext jxp = JXPathContext.newContext(docResult);
+        jxp.setLenient(true);
+        jxp.registerNamespace(QUERY_NAMESPACE_PREFIX, QUERY_NAMESPACE_URI);
+        jxp.registerNamespace(AkomaNtoso.AN_NAMESPACE_PREFIX, AkomaNtoso.AN_NAMESPACE_URI);
+
+        //assertions
+        String responseRequestAction = (String)jxp.getValue("/query:query/query:request/query:action");
+        assertEquals(queryAction, responseRequestAction);
+
+        String responseRequestUri = (String)jxp.getValue("/query:query/query:request/query:params/an:uri");
+        assertEquals(queryUri, responseRequestUri);
+
+        Iterator<DOMNodePointer> itPtrMatch = (Iterator<DOMNodePointer>)jxp.iteratePointers("/query:query/query:results/query:match");
+
+        DOMNodePointer ptrMatch = itPtrMatch.next();
+        assertEquals("/ke/act/1993-12-31/9/eng@/main.xml", getAttributeValue("an-manifestation-uri", ptrMatch));
+        assertEquals("/db/bungeni/data/ke/act/1993/12-16_9_eng_main.xml", getAttributeValue("db-uri", ptrMatch));
 
         try
         {

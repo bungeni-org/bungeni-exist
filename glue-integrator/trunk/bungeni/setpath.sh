a3e224b74f12659b2860a7729586301e5b39dfc7
aowino <aowino@25fa2e06-0413-11de-b4d5-6978306beb92>
2011-10-19 15:15:54 +0000

    Glue integrator initial files including Java Libraries.

---
+++ glue-integrator/trunk/bungeni/setpath.sh
@@ -0,0 +1,3 @@
+export JYTHON_PATH=/home/anthony/jython2.5.2/jython.jar
+export CLASSPATH=./lib/jaxen-1.1.1.jar:./lib/xercesImpl.jar:./lib/saxon9-dom.jar:./lib/saxon9.jar:./lib/saxon9-xpath.jar:./lib/saxon9-s9api.jar:./lib/dom4j-1.6.1.jar:./lib/log4j.jar:./lib/bungenirestlet.jar:./lib/editorextinterfaces.jar:./lib/editorplugininterface.jar:./lib/odttransformer.jar
+java -cp $JYTHON_PATH:$CLASSPATH org.python.util.jython ./glue.py 

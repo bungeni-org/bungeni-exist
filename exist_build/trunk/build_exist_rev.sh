d33b30fc6bc88ed87b8b70c5797bb232cc22fbea
ashok.hariharan <kohsah@users.noreply.github.com>
2013-01-07 12:19:47 +0000

    Updated exist build script to support 2.0 stable

--- exist_build/trunk/build_exist_rev.sh
+++ exist_build/trunk/build_exist_rev.sh
@@ -1,10 +1,70 @@
 
+function scriba_build {
+    echo "Setting up Scriba epub plugin"
+
+    SCRIBA_DIR=../tmp_scriba
+    SCRIBA_MODULE_DIR=$EXIST_INST_PATH/extensions/modules/src/org/exist/xquery/modules/epub
+    SCRIBA_XQ_MODULE="http://bungeni-exist.googlecode.com/svn/exist-scriba/trunk/epub"
+    SCRIBA_DOWN_FILE=scriba-ebook-maker.tar.gz
+
+    mkdir -p $SCRIBA_DIR
+    if [ -f $SCRIBA_DIR/$SCRIBA_DOWN_FILE ];
+    then
+        echo "Scriba module archive already exists, attempting to use existing one..."
+    else
+        echo "Downloading Scriba module archive..."
+        rm -rf  $SCRIBA_DIR/*
+        wget http://bungeni-exist.googlecode.com/files/scriba-ebook-maker.tar.gz -P  $SCRIBA_DIR
+    fi
+
+    tar xvf $SCRIBA_DIR/scriba-ebook-maker.tar.gz -C $SCRIBA_DIR --strip-components=1
+    echo "Copying license information"
+    cp -R $SCRIBA_DIR/lib/License $EXIST_INST_PATH/lib/user/scriba_license
+
+    echo "Copying scriba specific libraries"
+    cp  $SCRIBA_DIR/scriba-ebook-maker.jar $EXIST_INST_PATH/lib/user
+    cp  $SCRIBA_DIR/lib/bcprov-jdk16-146.jar $EXIST_INST_PATH/lib/user
+    cp  $SCRIBA_DIR/lib/boilerpipe-1.2.0.jar $EXIST_INST_PATH/lib/user
+    cp  $SCRIBA_DIR/lib/dom4j-2.0.0-ALPHA-2.jar $EXIST_INST_PATH/lib/user
+    cp  $SCRIBA_DIR/lib/epubcheck-1.2.jar $EXIST_INST_PATH/lib/user
+    cp  $SCRIBA_DIR/lib/Filters.jar $EXIST_INST_PATH/lib/user
+    cp  $SCRIBA_DIR/lib/fontbox-1.6.0.jar $EXIST_INST_PATH/lib/user
+    cp  $SCRIBA_DIR/lib/jai_imageio-1.1.jar $EXIST_INST_PATH/lib/user
+    cp  $SCRIBA_DIR/lib/java-image-scaling-0.8.5.jar $EXIST_INST_PATH/lib/user
+    cp  $SCRIBA_DIR/lib/jempbox-1.6.0.jar $EXIST_INST_PATH/lib/user
+    cp  $SCRIBA_DIR/lib/jing.jar $EXIST_INST_PATH/lib/user
+    cp  $SCRIBA_DIR/lib/jsoup-1.5.2.jar $EXIST_INST_PATH/lib/user
+    cp  $SCRIBA_DIR/lib/jtidy-r918.jar $EXIST_INST_PATH/lib/user
+    cp  $SCRIBA_DIR/lib/juniversalchardet-1.0.3.jar $EXIST_INST_PATH/lib/user
+    cp  $SCRIBA_DIR/lib/pdfbox-1.6.0.jar $EXIST_INST_PATH/lib/user
+    cp  $SCRIBA_DIR/lib/commons-cli-1.2.jar $EXIST_INST_PATH/lib/user
+
+    echo "Scriba Jar Ignore List :"
+    echo commons-codec-1.5.jar
+    echo commons-io-2.0.1.jar
+    echo log4j.jar
+    echo nekohtml-1.9.13.jar
+    echo saxon.jar
+    echo xalan.jar
+    echo xerces-2.9.1.jar
+
+    echo "Updating libraries in eXist used by Scriba"
+    echo "Adding commons-lang3 to eXist side by side with commons-lang 2.x"
+    cp  $SCRIBA_DIR/lib/commons-lang3-3.1.jar $EXIST_INST_PATH/lib/user
+
+    echo "Installing Scriba XQuery module"
+    svn export $SCRIBA_XQ_MODULE $EXIST_INST_PATH/extensions/modules/src/org/exist/xquery/modules/epub --force
+}
+
+
 if [ -z "$1" ]; then
   echo "You need at least specify a revision number"
+  echo "Syntax : ./build_exist_rev.sh <rev no> <folder with exist source> scriba" 
+  echo "Third parameter is optional ; <rev no> can be a number or HEAD "
   exit 91
 fi
 
-EXIST_REPO="svn://svn.code.sf.net/p/exist/code/trunk/eXist"
+EXIST_REPO="svn://svn.code.sf.net/p/exist/code/stable/eXist-2.0.x"
 # Exist Revision
 EXIST_REV=$1
 
@@ -24,6 +84,17 @@ else
       exit 98
   fi
 fi
+
+if [ -z "$3" ]; then
+  echo "Scriba will not be built"
+  SCRIBA_BUILD="NO"
+else
+  if [$3 == "scriba" ]; then
+    SCRIBA_BUILD="YES"
+  else
+    SCRIBA_BUILD="NO"
+  fi
+fi   
 
 if [ -z "$JAVA_HOME" ]; then
    echo "JAVA_HOME is not set ! Please set JAVA_HOME before executing this script"
@@ -57,60 +128,13 @@ sed -i 's|</builtin-modules>|<module uri="http://exist-db.org/xquery/epub" class
 echo "Enabling xslfo module"
 sed -i 's/include.module.xslfo = false/include.module.xslfo = true/g' ./extensions/build.properties
 
-echo "Setting up Scriba epub plugin"
-
-SCRIBA_DIR=../tmp_scriba
-SCRIBA_MODULE_DIR=$EXIST_INST_PATH/extensions/modules/src/org/exist/xquery/modules/epub
-SCRIBA_XQ_MODULE="http://bungeni-exist.googlecode.com/svn/exist-scriba/trunk/epub"
-SCRIBA_DOWN_FILE=scriba-ebook-maker.tar.gz
-
-mkdir -p $SCRIBA_DIR
-if [ -f $SCRIBA_DIR/$SCRIBA_DOWN_FILE ];
-then
-    echo "Scriba module archive already exists, attempting to use existing one..."
+if [ $SCRIBA_BUILD == "YES" ]; then
+  echo "Enabling Scriba build"
+  scriba_build
 else
-    echo "Downloading Scriba module archive..."
-    rm -rf  $SCRIBA_DIR/*
-    wget http://bungeni-exist.googlecode.com/files/scriba-ebook-maker.tar.gz -P  $SCRIBA_DIR
+  echo "Scriba is not included in this build"
 fi
 
-tar xvf $SCRIBA_DIR/scriba-ebook-maker.tar.gz -C $SCRIBA_DIR --strip-components=1
-echo "Copying license information"
-cp -R $SCRIBA_DIR/lib/License $EXIST_INST_PATH/lib/user/scriba_license
-
-echo "Copying scriba specific libraries"
-cp  $SCRIBA_DIR/scriba-ebook-maker.jar $EXIST_INST_PATH/lib/user
-cp  $SCRIBA_DIR/lib/bcprov-jdk16-146.jar $EXIST_INST_PATH/lib/user
-cp  $SCRIBA_DIR/lib/boilerpipe-1.2.0.jar $EXIST_INST_PATH/lib/user
-cp  $SCRIBA_DIR/lib/dom4j-2.0.0-ALPHA-2.jar $EXIST_INST_PATH/lib/user
-cp  $SCRIBA_DIR/lib/epubcheck-1.2.jar $EXIST_INST_PATH/lib/user
-cp  $SCRIBA_DIR/lib/Filters.jar $EXIST_INST_PATH/lib/user
-cp  $SCRIBA_DIR/lib/fontbox-1.6.0.jar $EXIST_INST_PATH/lib/user
-cp  $SCRIBA_DIR/lib/jai_imageio-1.1.jar $EXIST_INST_PATH/lib/user
-cp  $SCRIBA_DIR/lib/java-image-scaling-0.8.5.jar $EXIST_INST_PATH/lib/user
-cp  $SCRIBA_DIR/lib/jempbox-1.6.0.jar $EXIST_INST_PATH/lib/user
-cp  $SCRIBA_DIR/lib/jing.jar $EXIST_INST_PATH/lib/user
-cp  $SCRIBA_DIR/lib/jsoup-1.5.2.jar $EXIST_INST_PATH/lib/user
-cp  $SCRIBA_DIR/lib/jtidy-r918.jar $EXIST_INST_PATH/lib/user
-cp  $SCRIBA_DIR/lib/juniversalchardet-1.0.3.jar $EXIST_INST_PATH/lib/user
-cp  $SCRIBA_DIR/lib/pdfbox-1.6.0.jar $EXIST_INST_PATH/lib/user
-cp  $SCRIBA_DIR/lib/commons-cli-1.2.jar $EXIST_INST_PATH/lib/user
-
-echo "Scriba Jar Ignore List :"
-echo commons-codec-1.5.jar
-echo commons-io-2.0.1.jar
-echo log4j.jar
-echo nekohtml-1.9.13.jar
-echo saxon.jar
-echo xalan.jar
-echo xerces-2.9.1.jar
-
-echo "Updating libraries in eXist used by Scriba"
-echo "Adding commons-lang3 to eXist side by side with commons-lang 2.x"
-cp  $SCRIBA_DIR/lib/commons-lang3-3.1.jar $EXIST_INST_PATH/lib/user
-
-echo "Installing Scriba XQuery module"
-svn export $SCRIBA_XQ_MODULE $EXIST_INST_PATH/extensions/modules/src/org/exist/xquery/modules/epub --force
 echo "Removing .svn files from the exist installation folder"
 find . -name '.svn' -print0 | xargs -0 rm -rf 
 
